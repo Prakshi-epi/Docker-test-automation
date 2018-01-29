da07ad8688725f5c2a4a04465112635368b49809
Prakshi-epi <35418096+Prakshi-epi@users.noreply.github.com>
2018-01-29 16:41:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-	pip install --user awscli
+	pip install awscli
 	export PATH=$PATH:$HOME/.local/bin
 	eval $(aws ecr get-login --no-include-email --region us-east-1)
 	docker tag docker-wordcount:latest 075354070244.dkr.ecr.us-east-1.amazonaws.com/word-test:latest
