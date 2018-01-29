301e2c64253cd8a768d1a979973b0af6f0e691bc
Prakshi-epi <35418096+Prakshi-epi@users.noreply.github.com>
2018-01-29 14:06:53 +0530

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,7 @@
-print "wordcount"
+from collections import Counter
+fname = '/12.txt'
+fout = '/out.txt'
+with open(fname, 'r') as file:
+	wordcount = Counter(file.read().split())
+with open(fout, 'w') as newfile:
+	newfile.write(str(wordcount))
