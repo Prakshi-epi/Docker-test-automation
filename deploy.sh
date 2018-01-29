9a071e0f8d6a68738c236368b0a905a12359aa2e
Prakshi-epi <35418096+Prakshi-epi@users.noreply.github.com>
2018-01-29 16:37:02 +0530

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+	pip install --user awscli
+	export PATH=$PATH:$HOME/.local/bin
+	eval $(aws ecr get-login --no-include-email --region us-east-1)
+	docker tag docker-wordcount:latest 075354070244.dkr.ecr.us-east-1.amazonaws.com/word-test:latest
+	docker push 075354070244.dkr.ecr.us-east-1.amazonaws.com/word-test:latest
+fi
